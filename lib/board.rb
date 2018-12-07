15038b88286bac383828bd202847bb28c0c17cbd
Nicole D <nnardella@gmail.com>
2018-12-07 02:31:04 +0000

    Automatically backed up by Learn

---
+++ lib/board.rb
@@ -0,0 +1,32 @@
+require 'pry'
+class Board
+
+  attr_accessor :cells
+
+  def initialize
+    reset!
+  end
+
+  def reset!
+    @cells = Array.new(9, " ")
+  end
+
+  def display
+    puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
+    puts "-----------"
+    puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
+    puts "-----------"
+    puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
+  end
+
+  def position(input)
+   cells[input.to_i-1]
+   #takes the cells array and returns user_input ["O", " ", " ", " ", "X", " ", " ", " ", "X"]
+ end
+
+ def full?
+   @cells.all? { |cell|  cell == "X" || cell == "O"  }
+   #any? returns true if all conditions are meet.  
+ end
+
+end
